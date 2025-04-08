14f1b0817020a95e35c483befcc9d9e27cb9b638
Sachin-Dhuriya <sachindhuriya561@gmail.com>
2025-04-08 11:39:48 +0530

    Updated server.js

--- beinghr_Backend-main/server.js
+++ beinghr_Backend-main/server.js
@@ -266,6 +266,6 @@ app.get("/check-admin",async (req, res) => {
   });
 //--------------------------------------Listening Port----------------------------------------------------------------
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`App is listening on PORT ${PORT}...`);
 });
